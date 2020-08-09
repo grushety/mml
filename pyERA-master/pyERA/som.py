df1a6ea260871205c3867c2265ff6268dd94f389
grushety <53139875+grushety@users.noreply.github.com>
2020-08-09 21:50:55 +0200

    Add files via upload

--- pyERA-master/pyERA/som.py
+++ pyERA-master/pyERA/som.py
@@ -149,6 +149,22 @@ class Som:
             it.iternext()
         row, col = np.unravel_index(output_matrix.argmin(), output_matrix.shape)
         return (row, col)
+    
+    def return_BMU_index_cosine(self, input_vector):
+        """Return the coordinates of the BMU.
+
+        @param input_vector the vector to use for the comparison.
+        """
+        output_matrix = np.zeros((self._matrix_size, self._matrix_size))
+        it = np.nditer(output_matrix, flags=['multi_index'])
+        while not it.finished:
+            #print "%d <%s>" % (it[0], it.multi_index)
+            dist1 = self.return_euclidean_distance(input_vector, self._weights_matrix[it.multi_index[0], it.multi_index[1], :])
+            dist2 = self.return_cosine_similarity(input_vector, self._weights_matrix[it.multi_index[0], it.multi_index[1], :])
+            output_matrix[it.multi_index[0], it.multi_index[1]] = 0.5*(dist1 + 1-dist2)
+            it.iternext()
+        row, col = np.unravel_index(output_matrix.argmin(), output_matrix.shape)
+        return (row, col)
 
 
     def return_BMU_weights(self, input_vector):
